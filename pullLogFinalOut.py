877d77db0f24546153c187fb47e210bb0a00e942
j-tsai <jen.cy.tsai@gmail.com>
2015-03-13 12:29:27 -0700

    Create pullLogFinalOut.py

---
+++ pullLogFinalOut.py
@@ -0,0 +1,14 @@
+import os
+root = '/Volumes'
+f = open(r'LogFinalOut.txt','w')
+f.write('file\tnumInput\tuniquelyMapped\tuniquePercent\n')
+index = [5,8,9]
+for subdir, dirs, files in os.walk(root):
+    for file in files:
+        if file != 'Log.final.out':
+            continue
+        filename = os.path.join(subdir,file)
+        txt = open(os.path.join(subdir,file)).read().split('\n')
+        stat = '\t'.join([txt[i].strip().split('\t')[1] for i in index])
+        f.write(filename + '\t' + '\t'.join([txt[i].strip().split('\t')[1] for i in index]) + '\n')
+f.close()
